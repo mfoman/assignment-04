c6f3465d4c635f07d0ec24f6e8822538343bfbbd
mfoman <frederikblunck@gmail.com>
2022-10-06 20:07:27 +0200

    Workitems and tests implemented

--- Assignment.Infrastructure/WorkItem.cs
+++ Assignment.Infrastructure/WorkItem.cs
@@ -1,15 +1,22 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Assignment.Infrastructure;
 
 public class WorkItem
 {
+    [Key]
     public int Id { get; set; }
 
+    [Required, MaxLength(50)]
     public string Title { get; set; }
 
     public int? AssignedToId { get; set; }
 
     public User? AssignedTo { get; set; }
 
+    public string? Description { get; set; }
+
+    [Required]
     public State State { get; set; }
 
     public ICollection<Tag> Tags { get; set; }
@@ -19,4 +26,10 @@ public class WorkItem
         Title = title;
         Tags = new HashSet<Tag>();
     }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime? UpdatedDate { get; set; }
 }
--- Assignment.Infrastructure/WorkItemRepository.cs
+++ Assignment.Infrastructure/WorkItemRepository.cs
@@ -2,31 +2,135 @@ namespace Assignment.Infrastructure;
 
 public class WorkItemRepository : IWorkItemRepository
 {
-    private KanbanContext context;
+    private KanbanContext _context;
 
     public WorkItemRepository(KanbanContext context)
     {
-        this.context = context;
+        this._context = context;
     }
 
     public (Response Response, int ItemId) Create(WorkItemCreateDTO item)
     {
-        throw new NotImplementedException();
+        var entity = _context.Items.FirstOrDefault(c => c.Title == item.Title);
+        var user = _context.Users.Find(item.AssignedToId);
+
+        Response status;
+
+        if (user is null && item.AssignedToId != null)
+        {
+            entity = new WorkItem(item.Title)
+            {
+                AssignedTo = null,
+                State = State.Removed,
+                Tags = new List<Tag> { }
+            };
+
+            status = BadRequest;
+        }
+        else if (entity is null)
+        {
+            var tags = from c in _context.Tags
+                       where item.Tags.Contains(c.Name)
+                       select c;
+
+            entity = new WorkItem(item.Title)
+            {
+                AssignedTo = user,
+                Description = item.Description,
+                State = State.New,
+                Tags = tags.ToList()
+            };
+
+            _context.Items.Add(entity);
+            _context.SaveChanges();
+
+            status = Created;
+        }
+        else
+        {
+            status = Conflict;
+        }
+
+        var created = new WorkItemDTO(
+            entity.Id,
+            entity.Title ?? "",
+            entity.AssignedTo?.Name ?? "",
+            entity.Tags as IReadOnlyCollection<string> ?? new List<string> { },
+            entity.State
+        );
+
+        return (status, created.Id);
     }
 
     public Response Delete(int itemId)
     {
-        throw new NotImplementedException();
+        var task = _context.Items.FirstOrDefault(c => c.Id == itemId);
+        Response response;
+
+        if (task is null)
+        {
+            response = NotFound;
+        }
+        else if ((new[] { State.Closed, State.Removed, State.Resolved }).Contains(task.State))
+        {
+            response = Conflict;
+        }
+        else if (task.State == State.Active)
+        {
+            task.State = State.Removed;
+            _context.Items.Update(task);
+
+            response = Updated;
+        }
+        else
+        {
+            _context.Items.Remove(task);
+            _context.SaveChanges();
+
+            response = Deleted;
+        }
+
+        return response;
     }
 
     public WorkItemDetailsDTO Find(int itemId)
     {
-        throw new NotImplementedException();
+        var tasks = from c in _context.Items
+                    where c.Id == itemId
+                    select c;
+
+        var t = tasks.FirstOrDefault();
+
+        if (t is null)
+        {
+            return new WorkItemDetailsDTO(-1, "", "", DateTime.Now, "", new List<string> { }, State.Closed, DateTime.Now);
+        }
+
+        return new WorkItemDetailsDTO(
+            t.Id,
+            t.Title ?? "",
+            t.Description ?? "",
+            t.CreatedDate,
+            t.AssignedTo?.Name ?? "",
+            t.Tags as IReadOnlyCollection<string> ?? new List<string> { },
+            t.State,
+            t.UpdatedDate ?? DateTime.UtcNow
+        );
     }
 
     public IReadOnlyCollection<WorkItemDTO> Read()
     {
-        throw new NotImplementedException();
+        var Tasks = from c in _context.Items
+                    orderby c.Title
+                    select new WorkItemDTO(
+                        c.Id,
+                        c.Title ?? "",
+                        (c.AssignedTo == null ? "" : c.AssignedTo.Name ?? ""),
+                        c.Tags as IReadOnlyCollection<string> ?? new List<string> { },
+                        c.State
+                    );
+
+        return Tasks.ToArray();
     }
 
     public IReadOnlyCollection<WorkItemDTO> ReadByState(State state)
@@ -51,6 +155,35 @@ public class WorkItemRepository : IWorkItemRepository
 
     public Response Update(WorkItemUpdateDTO item)
     {
-        throw new NotImplementedException();
+        var entity = _context.Items.Find(item.Id);
+
+        Response response;
+
+        if (entity is null)
+        {
+            response = NotFound;
+        }
+        else if (_context.Items.FirstOrDefault(c => c.Id != item.Id && c.Title == item.Title) != null)
+        {
+            response = Conflict;
+        }
+        else
+        {
+            entity.Title = item.Title;
+            entity.Description = item.Description;
+            entity.State = item.State;
+            entity.Tags = item.Tags as List<Tag> ?? new List<Tag> { };
+
+            if (entity.AssignedTo?.Id != item.AssignedToId)
+            {
+                var user = _context.Users.Find(item.AssignedToId);
+                entity.AssignedTo = user;
+            }
+
+            _context.SaveChanges();
+            response = Updated;
+        }
+
+        return response;
     }
 }
