d6b8a178b33b947b7f07b17d3e28e44fb5a6ef90
mfoman <frederikblunck@gmail.com>
2022-10-06 19:53:28 +0200

    Add tags and tests for tags

--- Assignment.Infrastructure/Tag.cs
+++ Assignment.Infrastructure/Tag.cs
@@ -2,7 +2,10 @@ namespace Assignment.Infrastructure;
 
 public class Tag
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required, MaxLength(50)]
     public string Name { get; set; }
     public ICollection<WorkItem> WorkItems { get; set; }
 
--- Assignment.Infrastructure/TagRepository.cs
+++ Assignment.Infrastructure/TagRepository.cs
@@ -2,35 +2,100 @@ namespace Assignment.Infrastructure;
 
 public class TagRepository : ITagRepository
 {
-    private KanbanContext context;
+    private KanbanContext _context;
 
     public TagRepository(KanbanContext context)
     {
-        this.context = context;
+        this._context = context;
     }
 
     public (Response Response, int TagId) Create(TagCreateDTO tag)
     {
-        throw new NotImplementedException();
+        var entity = _context.Tags.FirstOrDefault(c => c.Name == tag.Name);
+        Response status;
+
+        if (entity is null)
+        {
+            entity = new Tag(tag.Name);
+
+            _context.Tags.Add(entity);
+            _context.SaveChanges();
+
+            status = Created;
+        }
+        else
+        {
+            status = Conflict;
+        }
+
+        var created = new TagDTO(entity.Id, entity.Name);
+
+        return (status, created.Id);
     }
 
     public Response Delete(int tagId, bool force = false)
     {
-        throw new NotImplementedException();
+        var tag = _context.Tags.FirstOrDefault(c => c.Id == tagId);
+        Response response;
+
+        if (tag is null)
+        {
+            response = NotFound;
+        }
+        else if (tag.WorkItems.Count != 0 && !force)
+        {
+            response = Conflict;
+        }
+        else
+        {
+            _context.Tags.Remove(tag);
+            _context.SaveChanges();
+
+            response = Deleted;
+        }
+
+        return response;
     }
 
-    public TagDTO Find(int tagId)
+    public TagDTO? Find(int tagId)
     {
-        throw new NotImplementedException();
+        var tags = from c in _context.Tags
+                   where c.Id == tagId
+                   select new TagDTO(c.Id, c.Name);
+
+        return tags.FirstOrDefault();
     }
 
     public IReadOnlyCollection<TagDTO> Read()
     {
-        throw new NotImplementedException();
+        var tags = from c in _context.Tags
+                   orderby c.Name
+                   select new TagDTO(c.Id, c.Name);
+
+        return tags.ToArray();
     }
 
     public Response Update(TagUpdateDTO tag)
     {
-        throw new NotImplementedException();
+        var entity = _context.Tags.Find(tag.Id);
+
+        Response response;
+
+        if (entity is null)
+        {
+            response = NotFound;
+        }
+        else if (_context.Tags.FirstOrDefault(c => c.Id != tag.Id && c.Name == tag.Name) != null)
+        {
+            response = Conflict;
+        }
+        else
+        {
+            entity.Name = tag.Name;
+            _context.SaveChanges();
+            response = Updated;
+        }
+
+        return response;
     }
 }
